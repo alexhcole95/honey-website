82a54959da201909582008effb9a2a6577a62b97
Alex Cole <alexhcole95@gmail.com>
2024-06-30 23:08:50 -0500

    commenting out .env

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,3 @@
 locals {
-  # useful if using a .env file
-  envs = { for tuple in regexall("(.*)=(.*)", file(".env")) : tuple[0] => sensitive(tuple[1]) }
+  # envs = { for tuple in regexall("(.*)=(.*)", file(".env")) : tuple[0] => sensitive(tuple[1]) }
 }
